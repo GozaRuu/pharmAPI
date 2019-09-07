ddd0d3213fc75d0a7f87483d2687ab6638dc6594
GozaRuu <kais.sghari@gmail.com>
2019-09-07 18:23:38 +0200

    update query

--- routes/medicaments.js
+++ routes/medicaments.js
@@ -5,7 +5,7 @@ const knex = require('../knexClient')
 /* GET users listing. */
 router.get('/:name', async (req, res) => {
   const medicament = req.params.name
-  const data = await knex.select('name').from('pharmacies')
+  const data = await knex.select('*').from('pharmacies')
 
   res.json(data)
 })
