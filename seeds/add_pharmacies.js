d3263b85081661efe9955429d6f189b3ecd4c964
GozaRuu <kais.sghari@gmail.com>
2019-09-08 10:51:55 +0200

    update seeds

--- seeds/add_pharmacies.js
+++ seeds/add_pharmacies.js
@@ -5,9 +5,10 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex('pharmacies').insert([
-        { name: 'Apotheke am Ostkreuz', long: '52.506242', lat: '13.466042' },
-        { name: 'Apotheke am Frankfurter Tor', long: '52.515811', lat: '13.450873' },
-        { name: 'apotheke victor', long: '22.4985734', lat: '23.9348378' }
+        { name: 'Pharmacie de Rivoli', long: '2.343760', lat: '48.860149' },
+        { name: 'Pharmacie de la Monnaie', long: '2.342569', lat: '48.859090' },
+        { name: 'Pharmacie du Parc Monceau', long: '2.305803', lat: '48.877459' },
+        { name: 'Pharmacie Batignolles Monceau', long: '2.315710', lat: '48.884846' }
       ])
     })
 }
--- seeds/pharmacies_medicaments_seed.js
+++ seeds/pharmacies_medicaments_seed.js
@@ -1,16 +1,15 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('pharmacy_medicament').del()
-    .then(function () {
+  return knex('pharmacy_medicament')
+    .del()
+    .then(function() {
       // Inserts seed entries
       return knex('pharmacy_medicament').insert([
-        {pharmacyId: 1, medicamentId: 1, count: '20', price: '30'},
-        {pharmacyId: 1, medicamentId: 2, count: '32', price: '50'},
-        {pharmacyId: 2, medicamentId: 1, count: '0', price: '32'},
-        {pharmacyId: 2, medicamentId: 2, count: '2', price: '50'},
-        {pharmacyId: 3, medicamentId: 1, count: '90', price:'39'},
-        {pharmacyId: 3, medicamentId: 2, count: '0', price: '49'},
-      ]);
-    });
-};
+        { pharmacyId: 1, medicamentId: 1, count: '20', price: '30' },
+        { pharmacyId: 2, medicamentId: 1, count: '0', price: '30.8' },
+        { pharmacyId: 3, medicamentId: 1, count: '0', price: '39' },
+        { pharmacyId: 3, medicamentId: 2, count: '76', price: '70' },
+        { pharmacyId: 4, medicamentId: 2, count: '33', price: '70.2' }
+      ])
+    })
+}
